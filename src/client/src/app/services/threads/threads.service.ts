81b05b07ff801b25ab7618e03e75edc5e492f8f7
Nicholas <stug4444@hotmail.com>
2021-06-28 18:30:22 -0400

    Search bar is working waiting on back end

---
+++ src/client/src/app/services/threads/threads.service.ts
@@ -0,0 +1,23 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { catchError, map } from 'rxjs/operators';
+import { RestApiService } from '../rest-api/rest-api.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ThreadsService {
+
+  constructor(private http: HttpClient,
+    private _restApiService: RestApiService) { }
+
+  searchThread(query: String): Observable<{}> {
+    var url = "/api/user/updateuser";
+
+    return this.http.put(url, query, {observe: 'response'}).pipe(
+      map(this._restApiService.getData),
+      catchError(err => this._restApiService.handleError(err))
+    );
+  }
+}
--- src/client/src/app/shared/components/header/header.component.ts
+++ src/client/src/app/shared/components/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { SearchService } from 'src/app/services/search/search.service';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +9,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
-
+  searchQuery = "";
+  constructor(private routerService: Router, private _searchService: SearchService) {
+    
+  }
+  search(t: any){
+    this._searchService.search(this.searchQuery);
+  }
   ngOnInit(): void {
+    console.log(this.routerService.getCurrentNavigation());
+    
   }
 
 }
