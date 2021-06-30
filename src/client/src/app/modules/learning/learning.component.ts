dfe8f94e47ee457fbb1730da12a99ff6d6c75c28
Jack McKinney <jack_mckinney98@hotmail.com>
2021-06-30 14:08:43 -0400

    BRUT47 implemented category list HTML

--- src/client/src/app/modules/learning/learning.component.ts
+++ src/client/src/app/modules/learning/learning.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./learning.component.scss']
 })
 export class LearningComponent implements OnInit {
-
+  categories = ["Accounting", "Current Affairs", "Entrepreneurship", "Human Resources", "Technology", "For Fun", "Movies"]
   constructor() { }
 
   ngOnInit(): void {
