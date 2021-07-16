ba9e2537252ee284fc64d4f852271984c9b455c3
Jack McKinney <jack_mckinney98@hotmail.com>
2021-07-16 17:42:32 -0400

    BRUT4 added search functionality to e learning video page

--- src/client/src/app/services/search/search.service.ts
+++ src/client/src/app/services/search/search.service.ts
@@ -2,14 +2,35 @@ import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
 import { User } from 'src/app/classes/user/user';
 import { ThreadService } from '../thread/thread.service';
+import { VideoService } from '../video/video.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SearchService {
 
-  constructor(private _threadService: ThreadService) { }
+  constructor(private _threadService: ThreadService, private _videoService: VideoService) { }
   threadSearchResults: BehaviorSubject<User[]> = new BehaviorSubject<User[]>([]);
+  videoSearchResults: BehaviorSubject<User[]> = new BehaviorSubject<User[]>([]);
+
+  public getVideoSearchResults$() {
+    return this.videoSearchResults.asObservable();
+  }
+  
+  public searchVideo(params: any, category: string) {
+    this._videoService.searchVideo(params, category).subscribe((res: any) => {
+      if(res.body === undefined) {
+        this.videoSearchResults.next([]);
+        return;
+      }
+      
+      res?.body.forEach((element: any) => {
+        delete element._id;
+      });
+
+      this.videoSearchResults.next(res?.body);
+    });
+  }  
 
   public getThreadSearchResults$() {
     return this.threadSearchResults.asObservable();
