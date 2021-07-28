f4d8410338de63bbfcd5b85a929bad4ba4cba33b
Joshua Carrasco Sousa <52802625+jarbihc@users.noreply.github.com>
2021-07-28 01:22:43 -0400

    BRUT-87 Fixed config bug

--- src/server/config/production.js
+++ src/server/config/production.js
@@ -3,12 +3,5 @@ module.exports = {
     COOKIE_OPTIONS: {
         httpOnly: true,
         sameSite: 'none'
-    },
-    CORS_MIDDLEWARE: function (req, res, next) {
-        res.setHeader('Access-Control-Allow-Origin', this.CORS_ORIGIN);
-        res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-        res.header('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept');
-        res.setHeader('Access-Control-Allow-Credentials', true);
-        next();
     }
 }
