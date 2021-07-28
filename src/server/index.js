b7e9eb7f26333f8d534d9a5c7d8ca4a3ef8895af
Ubuntu <ubuntu@ip-172-31-21-101.us-east-2.compute.internal>
2021-07-28 04:49:51 +0000

    BRUT-87 CORS bug fix

--- src/server/index.js
+++ src/server/index.js
@@ -13,7 +13,29 @@ if(process.env.NODE_ENV === "production") {
         origin: 'http://asdy.joshuacarrasco.com',
         optionsSuccessStatus: 200
     }
-    app.options('*', cors(corsOptions));
+    console.log("here");
+    //app.use(cors({origin: 'http://asdy.joshuacarrasco.com', credentials: true}));
+    app.use(function (req, res, next) {
+        // Website you wish to allow to connect
+        res.setHeader('Access-Control-Allow-Origin', 'http://asdy.joshuacarrasco.com');
+
+        // Request methods you wish to allow
+        res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+        // Request headers you wish to allow
+        res.header('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept');
+        // Set to true if you need the website to include cookies in the requests sent
+        // to the API (e.g. in case you use sessions)
+        res.setHeader('Access-Control-Allow-Credentials', true);
+
+//	res.setHeader('Access-Control-Expose-Headers', 'Authorization');
+	//res.setHeader('Allow-Origin-With-Credentials', true);
+
+        // Pass to next layer of middleware
+        next();
+    });
+
+    //app.use(cors({origin: 'http://asdy.joshuacarrasco.com', credentials: true}));
 }
 
 app.use(express.json());
